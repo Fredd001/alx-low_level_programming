618a1b05e0fc473caf782543a1940acb26f16ea0
Frederick Essien <fredessien20@gmail.com>
2023-02-17 13:12:03 -0800

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,6 @@
 /**
  * main - entry point
  *
- *
  * Return: always 0 (success) 
  */
 int main(void)
@@ -14,13 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n ==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else 
 		printf("%d is negative\n", n);
 	return (0);
 }
-
